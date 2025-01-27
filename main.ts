48f83cefc656e96542bfe83eac47a22fdc603742
Leo Hoerdemann <leo.h.trip@gmail.com>
2025-01-26 19:27:45 -0600

    basic functionality done

--- main.ts
+++ main.ts
@@ -1,81 +1,77 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting, TFile } from 'obsidian';
 
 // Remember to rename these classes and interfaces!
 
 interface MyPluginSettings {
-	mySetting: string;
+	metadataFeature: boolean;
+	zoomRange: number;
+
 }
 
 const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
+	metadataFeature: true,
+	zoomRange: 5,
 }
 
 export default class MyPlugin extends Plugin {
 	settings: MyPluginSettings;
+	originalMtimes: Map<string, number> = new Map();
+
 
 	async onload() {
 		await this.loadSettings();
 
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
-
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
-
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
-
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
-			}
-		});
-
 		// This adds a settings tab so the user can configure various aspects of the plugin
 		this.addSettingTab(new SampleSettingTab(this.app, this));
 
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
+		const win = (window as any).require('electron').remote.getCurrentWindow();
 
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
+		const webFrame = (window as any).require('electron').webFrame;
+
+		if (webFrame) {
+			webFrame.setVisualZoomLevelLimits(1, this.settings.zoomRange);
+		}
+
+
+		// Clean up when plugin is disabled
+        this.register(() => {
+            if (webFrame) {
+                webFrame.setVisualZoomLevelLimits(1, 1); // Reset to default
+            }
+        });
+
+
+		// removes obsidian from changing the last modified date on just opening a file
+
+		
+		// Register event to capture original mtime when file is opened
+        this.registerEvent(
+            this.app.workspace.on('file-open', (file: TFile) => {
+                if (file && this.settings.metadataFeature) {
+                    this.originalMtimes.set(file.path, file.stat.mtime);
+                }
+            })
+        );
+
+        // Register event to check content changes before saving
+        this.registerEvent(
+            this.app.vault.on('modify', async (file: TFile) => {
+                if (!this.settings.metadataFeature) return;
+
+                const originalContent = await this.app.vault.read(file);
+                const originalMtime = this.originalMtimes.get(file.path);
+
+                if (originalMtime && file.stat.mtime !== originalMtime) {
+                    // Only preserve mtime if content hasn't changed
+                    const currentContent = await this.app.vault.read(file);
+                    if (originalContent === currentContent) {
+                        // Use Node.js fs to directly set mtime
+						const fsPath = this.app.vault.adapter.getResourcePath(file.path);
+                        await window.require('fs').promises.utimes(fsPath, new Date(), new Date(originalMtime));
+                    }
+                }
+            })
+        );
 	}
 
 	onunload() {
@@ -88,22 +84,6 @@ export default class MyPlugin extends Plugin {
 
 	async saveSettings() {
 		await this.saveData(this.settings);
-	}
-}
-
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
-
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
-
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
 	}
 }
 
@@ -121,14 +101,29 @@ class SampleSettingTab extends PluginSettingTab {
 		containerEl.empty();
 
 		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
+			.setName('Metadata Feature')
+			.setDesc('Enable the metadata feature')
+			.addToggle(cb => cb
+				.setValue(this.plugin.settings.metadataFeature)
 				.onChange(async (value) => {
-					this.plugin.settings.mySetting = value;
+					this.plugin.settings.metadataFeature = value;
 					await this.plugin.saveSettings();
 				}));
+
+		new Setting(containerEl)
+			.setName('Maximum Zoom Level')
+			.setDesc('Set the maximum zoom level (1-8)')
+			.addSlider(slider => slider
+				.setLimits(1, 8, 0.5)
+				.setDynamicTooltip()
+				.setValue(this.plugin.settings.zoomRange)
+				.onChange(async (value) => {
+					this.plugin.settings.zoomRange = value;
+					const webFrame = (window as any).require('electron').webFrame;
+					if (webFrame) {
+						webFrame.setVisualZoomLevelLimits(1, value);
+					}
+				}));
+
 	}
 }
